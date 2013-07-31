46a1724d4bd2ae9ba0369d8f442a9fb8d598e483
Steven Kirk <grokys@gmail.com>
2013-07-31 02:31:38 +0200

    Removed some tabs, an empty file.

--- Avalonia/Media/Matrix.cs
+++ Avalonia/Media/Matrix.cs
@@ -1,29 +1,4 @@
-﻿// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-// Copyright (c) 2007 Novell, Inc. (http://www.novell.com)
-//
-// Authors:
-//	Chris Toshok (toshok@ximian.com)
-//
-
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Windows.Markup;
 
@@ -193,7 +168,6 @@ namespace Avalonia.Media
 
         public void Rotate(double angle)
         {
-            // R_theta==[costheta -sintheta; sintheta costheta],	
             double theta = angle * Math.PI / 180;
 
             Matrix r_theta = new Matrix(Math.Cos(theta), Math.Sin(theta),

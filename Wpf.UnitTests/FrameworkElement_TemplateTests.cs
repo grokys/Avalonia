6172b42c61d43f191219194faf02e0da1911290d
grokys <grokys@gmail.com>
2013-10-09 19:26:29 +0200

    Added TemplatedParent tests.
    To confirm that TemplatedParent is set on the whole templated tree, not
    just the root element.

---
+++ Wpf.UnitTests/FrameworkElement_TemplateTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Avalonia.UnitTests
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FrameworkElement_TemplateTests
+    {
+        [TestMethod]
+        public void TemplatedParent_Should_Be_Set_On_Template_Root()
+        {
+            Window target = new Window();
+            Application app = new Application();
+            bool passed = false;
+
+            target.Loaded += (s, e) =>
+            {
+                FrameworkElement templateRoot = VisualTreeHelper.GetChild(target, 0) as FrameworkElement;
+                passed = templateRoot.TemplatedParent == target;
+                app.Shutdown();
+            };
+
+            app.Run(target);
+
+            Assert.IsTrue(passed);
+        }
+
+        [TestMethod]
+        public void TemplatedParent_Should_Be_Set_On_Template_Child()
+        {
+            Window target = new Window();
+            Application app = new Application();
+            bool passed = false;
+
+            target.Loaded += (s, e) =>
+            {
+                FrameworkElement templateRoot = VisualTreeHelper.GetChild(target, 0) as FrameworkElement;
+                FrameworkElement templateChild = VisualTreeHelper.GetChild(templateRoot, 0) as FrameworkElement;
+                templateChild = VisualTreeHelper.GetChild(templateChild, 0) as FrameworkElement;
+                passed = templateChild.TemplatedParent == target;
+                app.Shutdown();
+            };
+
+            app.Run(target);
+
+            Assert.IsTrue(passed);
+        }
+    }
+}
